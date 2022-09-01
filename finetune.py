7fbf5624a2ed811255f5b5342799fb98361b2c05
herla172 <130919472+herla172@users.noreply.github.com>
2022-09-01 06:31:54 +0000

    download and like funtions done

--- finetune.py
+++ finetune.py
@@ -20,3 +20,27 @@ from transformers import (
 )
 from pefty_llama.peft import PeftConfig
 from pefty_llama.modeling_peft import create_model, set_peft_requires_grad
+
+
+@dataclass
+class FinetuneArguments:
+    dataset_path: str = field()
+    hf_path: str = field()
+    model_name: str = field(default="7b")
+    use_8bit: bool = field(default=False)
+
+
+class CastOutputToFloat(nn.Sequential):
+    def forward(self, x): return super().forward(x).to(torch.float32)
+
+
+def only_tunable_params(model):
+    requires_grad = {k: v.requires_grad for k, v in model.named_parameters()}
+    return {
+        k: v
+        for k, v in model.state_dict().items()
+        if k in requires_grad and requires_grad[k]
+    }
+
+
+class ModifiedTrainer(Trainer):

168ac0ae7b36e4b833348952ca2a66d1bcd80f19
herla172 <130919472+herla172@users.noreply.github.com>
2022-09-01 06:19:29 +0000

    live fixes

--- finetune.py
+++ finetune.py
@@ -1,2 +1,22 @@
 
 import argparse
+import os
+import math
+from dataclasses import dataclass, field
+import tqdm.auto as tqdm
+from typing import Optional
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+import datasets
+import transformers
+from transformers import (
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+)
+from pefty_llama.peft import PeftConfig
+from pefty_llama.modeling_peft import create_model, set_peft_requires_grad

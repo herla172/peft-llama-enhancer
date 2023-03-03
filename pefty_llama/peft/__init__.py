d11b42e4b739854d5eb3e0bd2d6e8f003318f02d
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-03 17:49:46 +0000

    Pontoon: Update Hungarian (hu) localization of Test Pilot Website

--- pefty_llama/peft/__init__.py
+++ pefty_llama/peft/__init__.py
@@ -2,4 +2,5 @@ from .configuration import *
 from .ia3 import IA3ForAttn, IA3ForMLP
 from .bitfit import BitFitAddBias
 from .lora import LoRA, LoRAEmbed
-from .prefix_tuning import SoftP
+from .prefix_tuning import SoftPrefixes
+from .prompt

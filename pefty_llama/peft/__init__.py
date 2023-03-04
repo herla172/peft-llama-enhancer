7ffeb9a0068cc60e7ce6d39e8376d6555a21c6db
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-04 08:55:35 +0000

    net/eth/ibmveth: Fixup retrieval of MAC address

--- pefty_llama/peft/__init__.py
+++ pefty_llama/peft/__init__.py
@@ -4,4 +4,5 @@ from .bitfit import BitFitAddBias
 from .lora import LoRA, LoRAEmbed
 from .prefix_tuning import SoftPrefixes
 from .prompt_tuning import AddSoftPrompt
-from .adapter i
+from .adapter import Adapter
+from .prefix

3ef6894023a252d3b17fe5d59fcdeeb5b7ed71e3
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-21 16:42:05 +0000

    ignore build

--- pefty_llama/peft/configuration.py
+++ pefty_llama/peft/configuration.py
@@ -21,3 +21,26 @@ ADAPTER_VERSION_PFEIFFER = "pfeiffer"
 class PeftConfig:
     peft_mode: str = field()
     peft_dtype: Any = field(default=torch.float32)
+
+    # Used by prompt, prefix, prefix_adapter
+    num_prefix_tokens: int = field(default=16)
+
+    # Prefix
+    prefix_use_mlp: bool = field(default=True)
+    prefix_mlp_intermediate_size: int = field(default=None)
+
+    # LoRA
+    lora_rank: int = field(default=8)
+    lora_alpha: int = field(default=16)
+    lora_mlp: bool = field(default=False)
+    lora_embedding: bool = field(default=False)
+
+    # Adapter
+    adapter_hidden_size: int = field(default=64)
+    adapter_version: str = field(default=ADAPTER_VERSION_PFEIFFER)  # houlsby, pfeiffer
+
+    def check(self):
+        assert self.peft_mode in (
+            PEFT_PREFIX, PEFT_PREFIX_ADAPTER, PEFT_PROMPT, PEFT_ADAPTER,
+            PEFT_IA3, PEFT_BITFIT,
+            NO_PEFT,

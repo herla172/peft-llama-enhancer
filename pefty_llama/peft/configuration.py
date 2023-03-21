cf4e49a338c82c3ba5c3c732bf4e909622a49bea
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-21 11:11:21 +0000

    Bump version: 1.0.0 → 1.0.1

--- pefty_llama/peft/configuration.py
+++ pefty_llama/peft/configuration.py
@@ -6,3 +6,18 @@ import torch
 
 PEFT_PREFIX = "prefix"
 PEFT_PROMPT = "prompt"
+PEFT_ADAPTER = "adapter"
+PEFT_PREFIX_ADAPTER = "prefix_adapter"
+PEFT_LORA = "lora"
+PEFT_IA3 = "ia3"
+PEFT_BITFIT = "bitfit"
+NO_PEFT = "nothing"
+
+ADAPTER_VERSION_HOULSBY = "houlsby"
+ADAPTER_VERSION_PFEIFFER = "pfeiffer"
+
+
+@dataclass
+class PeftConfig:
+    peft_mode: str = field()
+    peft_dtype: Any = field(default=torch.float32)

9c242acd17adc9015e55a05aeb287f9a424fe01a
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-21 06:35:50 +0000

    hacker rank

--- pefty_llama/peft/bitfit.py
+++ pefty_llama/peft/bitfit.py
@@ -11,4 +11,4 @@ class BitFitAddBias(nn.Module):
 
     def forward(self, hidden_state):
         input_dtype = hidden_state.dtype
-        return (hidden_state.
+        return (hidden_state.to(self.peft_con

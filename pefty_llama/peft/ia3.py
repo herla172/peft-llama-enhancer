b079967760c2551255bd8b6c83aab9bc168bd69d
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-24 07:02:04 +0000

    Docs...

--- pefty_llama/peft/ia3.py
+++ pefty_llama/peft/ia3.py
@@ -16,3 +16,27 @@ class IA3Attention(nn.Module):
         super().__init__()
         self.config = config
         self.n_heads = config.n_heads
+        self.head_dim = config.dim // config.n_heads
+
+        if config.use_8bit:
+            self.q_proj = NoInit8bitLinear(config.dim, config.dim, bias=False, threshold=6.0, has_fp16_weights=False)
+            self.k_proj = NoInit8bitLinear(config.dim, config.dim, bias=False, threshold=6.0, has_fp16_weights=False)
+            self.v_proj = NoInit8bitLinear(config.dim, config.dim, bias=False, threshold=6.0, has_fp16_weights=False)
+            self.o_proj = NoInit8bitLinear(config.dim, config.dim, bias=False, threshold=6.0, has_fp16_weights=False)
+        else:
+            self.q_proj = NoInitLinear(config.dim, config.dim, bias=False, dtype=config.dtype)
+            self.k_proj = NoInitLinear(config.dim, config.dim, bias=False, dtype=config.dtype)
+            self.v_proj = NoInitLinear(config.dim, config.dim, bias=False, dtype=config.dtype)
+            self.o_proj = NoInitLinear(config.dim, config.dim, bias=False, dtype=config.dtype)
+        self.rotary_emb = RotaryEmbedding(dim=self.head_dim)
+
+        # IA3-specific parameters:
+        self.peft_l_k = nn.Parameter(torch.ones(1, self.n_heads, 1, self.head_dim, dtype=config.dtype))
+        self.peft_l_v = nn.Parameter(torch.ones(1, self.n_heads, 1, self.head_dim, dtype=config.dtype))
+
+    def forward(self, hidden_states, attention_mask, cos, sin, kv_cache=None):
+        """
+        precomputed_kv_hidden_states is for init (pre-compute KV activations, e.g. for added prefixes)
+        kv_cache is for generation (cached past KV)
+        """
+        batch_size, q_seq_len, hidden_dim = hidden_states.size()

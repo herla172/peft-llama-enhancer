21dc31ec734eb106b006a13db9cd9b6f781a11b7
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-28 14:40:09 +0000

    added parameters

--- pefty_llama/peft/prefix_adapter.py
+++ pefty_llama/peft/prefix_adapter.py
@@ -2,4 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from pefty_llama.configuration import LLaMAConfig
-from .configuration import 
+from .configuration import PeftConfig
+
+
+class Pre

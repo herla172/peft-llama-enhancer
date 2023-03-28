b1b15a16fad5dd1073e65f83830b3202aa596fe9
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-28 20:37:01 +0000

    Some code formatting

--- pefty_llama/peft/prefix_adapter.py
+++ pefty_llama/peft/prefix_adapter.py
@@ -6,4 +6,5 @@ from .configuration import PeftConfig
 
 
 class PrefixAdapter(nn.Module):
-    def __init__(self, config: LLaMAConfig, peft_con
+    def __init__(self, config: LLaMAConfig, peft_config: PeftConfig):
+    

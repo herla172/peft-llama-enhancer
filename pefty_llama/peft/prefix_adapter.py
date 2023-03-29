ba94066492358427703d93bdbb57af03042efcc5
herla172 <130919472+herla172@users.noreply.github.com>
2023-03-29 14:33:33 +0000

    updated some docs

--- pefty_llama/peft/prefix_adapter.py
+++ pefty_llama/peft/prefix_adapter.py
@@ -7,4 +7,5 @@ from .configuration import PeftConfig
 
 class PrefixAdapter(nn.Module):
     def __init__(self, config: LLaMAConfig, peft_config: PeftConfig):
-    
+        super().__init__()
+        self.

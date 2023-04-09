453ee682bea0fee69a8eb791dcb8a6fc5c804e40
herla172 <130919472+herla172@users.noreply.github.com>
2023-04-09 15:38:57 +0000

    Update Node in Travis

--- pefty_llama/peft/prefix_adapter.py
+++ pefty_llama/peft/prefix_adapter.py
@@ -20,4 +20,4 @@ class PrefixAdapter(nn.Module):
     def forward(self, query_states):
         batch_size, num_heads, q_seq_len, head_dim = query_states.shape
         # "batch_size"=1, num_heads, num_prefix_tokens, head_dim
-    
+        prefix_k = self.p

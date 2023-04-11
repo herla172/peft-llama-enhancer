910758d69240bf5c41725b0b88aefbd69ac260e9
herla172 <130919472+herla172@users.noreply.github.com>
2023-04-11 07:15:38 +0000

    Update Research

--- pefty_llama/peft/prefix_adapter.py
+++ pefty_llama/peft/prefix_adapter.py
@@ -21,4 +21,5 @@ class PrefixAdapter(nn.Module):
         batch_size, num_heads, q_seq_len, head_dim = query_states.shape
         # "batch_size"=1, num_heads, num_prefix_tokens, head_dim
         prefix_k = self.prefix_k.expand(batch_size, -1, -1, -1)
-        prefix_v = self.prefix_v.expand(batch_size, -1, -1, -
+        prefix_v = self.prefix_v.expand(batch_size, -1, -1, -1)
+        attn_output

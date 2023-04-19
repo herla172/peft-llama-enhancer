2fa6c8cf34b435b773d990fad64f687dbb77ec7f
herla172 <130919472+herla172@users.noreply.github.com>
2023-04-19 06:33:25 +0000

    wrong name

--- pefty_llama/peft/prompt_tuning.py
+++ pefty_llama/peft/prompt_tuning.py
@@ -8,3 +8,4 @@ class AddSoftPrompt(nn.Module):
     def __init__(self, config: LLaMAConfig, peft_config: PeftConfig):
         super().__init__()
         self.peft_config = peft_config
+        self.soft_prompt 

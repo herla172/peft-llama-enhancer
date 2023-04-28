41d46348cf1f5321a1f4b440d0f6a828003fdafe
herla172 <130919472+herla172@users.noreply.github.com>
2023-04-28 17:25:35 +0000

    Minor tidying up

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open("requirements.txt", "r") as f
+with open("requirements.txt", "r") as f:
+    requires = f.read()

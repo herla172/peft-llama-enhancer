3f544ead42e7da3cd61193139fc19e239fd53a9b
herla172 <130919472+herla172@users.noreply.github.com>
2023-05-12 06:43:07 +0000

    Markdown README

--- tokenize_dataset.py
+++ tokenize_dataset.py
@@ -9,4 +9,4 @@ import transformers
 
 
 def read_jsonl(path):
-    # Manually open because .splitlines is different fro
+    # Manually open because .splitlines is different from iterating over line

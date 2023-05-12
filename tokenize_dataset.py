68c30e0f07fde20612765562272ab4b67c725cf2
herla172 <130919472+herla172@users.noreply.github.com>
2023-05-12 07:31:14 +0000

    Labels

--- tokenize_dataset.py
+++ tokenize_dataset.py
@@ -9,4 +9,5 @@ import transformers
 
 
 def read_jsonl(path):
-    # Manually open because .splitlines is different from iterating over line
+    # Manually open because .splitlines is different from iterating over lines
+    with open(pat
